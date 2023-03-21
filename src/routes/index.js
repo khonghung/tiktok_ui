00df2a66fc00b9936d3900d4bd3a1f14a3e81057
khonghung <ahihimaianh3@gmail.com>
2023-03-21 11:10:50 +0700

    routes & setup layout

---
+++ src/routes/index.js
@@ -0,0 +1,19 @@
+// Layout Header only
+import { HeaderOnly } from '~/components/Layout';
+
+// Page
+import Home from '~/pages/Home';
+import Following from '~/pages/Following';
+import Upload from '~/pages/Upload';
+import Search from '~/pages/Search';
+
+export const publicRoutes = [
+    { path: '/', component: Home },
+    { path: '/following', component: Following },
+    { path: '/upload', component: Upload, layout: HeaderOnly },
+    { path: '/search', component: Search, layout: null },
+]
+
+export const privateRoutes = [
+
+]
