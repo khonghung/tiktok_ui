496000af1766bd353f0ceee17e40b3d0f4210175
khonghung <ahihimaianh3@gmail.com>
2023-03-22 17:38:01 +0700

    setup layout 1

--- src/components/Layouts/components/Header/index.js
+++ src/components/Layouts/components/Header/index.js
@@ -1,5 +1,9 @@
 import classNames from 'classnames/bind';
-import styles from './Header.module.scss'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleXmark, faMagnifyingGlass, faSpinner } from '@fortawesome/free-solid-svg-icons';
+
+import styles from './Header.module.scss';
+import image from '~/assets/images';
 
 const cx = classNames.bind(styles);
 
@@ -7,7 +11,22 @@ function Header() {
     return (
         <header className={cx('wrapper')}>
             <div className={cx('inner')}>
-                
+                <div className={cx('logo')}>
+                    <img src={image.logo} alt='Logo Tiktok' />
+                </div>
+                <div className={cx('search')}>
+                    <input placeholder='Search' spellCheck={false} />
+                    <button className={cx('clear')}>
+                        <FontAwesomeIcon icon={faCircleXmark} />
+                    </button>
+                    <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />
+                    <button className={cx('search-btn')}>
+                        <FontAwesomeIcon icon={faMagnifyingGlass} />
+                    </button>
+                </div>
+                <div className={cx('actions')}>
+                    <h2>Action</h2>
+                </div>
             </div>
         </header>
     );
