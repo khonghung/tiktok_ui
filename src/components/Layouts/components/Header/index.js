e838b3f99c42b6a773a79d2337c0096199a4905e
khonghung <ahihimaianh3@gmail.com>
2023-03-28 10:26:29 +0700

    setup layout 6

--- src/components/Layouts/components/Header/index.js
+++ src/components/Layouts/components/Header/index.js
@@ -1,20 +1,35 @@
 import { useEffect, useState } from 'react';
 import classNames from 'classnames/bind';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCircleQuestion, faCircleXmark, faEllipsisVertical, faKeyboard, faLanguage, faMagnifyingGlass, faPlus, faSpinner } from '@fortawesome/free-solid-svg-icons';
+import {
+    faBitcoinSign,
+    faCircleQuestion,
+    faCircleXmark,
+    faEllipsisVertical,
+    faGear,
+    faKeyboard,
+    faLanguage,
+    faMagnifyingGlass,
+    faPlus,
+    faSignOut,
+    faSpinner,
+    faUser
+} from '@fortawesome/free-solid-svg-icons';
 import Tippy from '@tippyjs/react/headless';
 // import 'tippy.js/dist/tippy.css'; // optional
 
 import styles from './Header.module.scss';
 import image from '~/assets/images';
 import { Wrapper as PopperWrapper } from '~/components/Popper';
-import AccountItem from '~/components/AccountItem';
+// import AccountItem from '~/components/AccountItem';
 import Button from '~/components/Button';
 import Menu from '~/components/Popper/Menu';
 
 const cx = classNames.bind(styles);
 
-const MENU_ITEM = [
+const currentUser = true;
+
+const MENU_ITEMS = [
     {
         icon: <FontAwesomeIcon icon={faLanguage} />,
         title: 'languages',
@@ -56,10 +71,35 @@ function Header() {
         switch (MenuItem.title) {
             case 'feedback':
                 // logic handle
-            break
+                break
             default:
         }
-    } 
+    }
+
+    const userMenu = [
+        {
+            icon: <FontAwesomeIcon icon={faUser} />,
+            title: 'View profile',
+            to: '/@hungkm',
+        },
+        {
+            icon: <FontAwesomeIcon icon={faBitcoinSign} />,
+            title: 'Coin',
+            to: '/coin',
+        },
+        {
+            icon: <FontAwesomeIcon icon={faGear} />,
+            title: 'Setting',
+            to: '/setting',
+        },
+        ...MENU_ITEMS,
+        {
+            icon: <FontAwesomeIcon icon={faSignOut} />,
+            title: 'Log Out',
+            to: '/logout',
+            separate: true,
+        },
+    ];
 
     return (
         <header className={cx('wrapper')}>
@@ -92,18 +132,31 @@ function Header() {
                         </button>
                     </div>
                 </Tippy>
-                <div className={cx('actions')}>
-                    <Button text leftIcon={<FontAwesomeIcon icon={faPlus} />}>Upload</Button>
-                    <Button primary>Login</Button>
 
-                    <Menu items={MENU_ITEM} onChange={handleMenuChange}>
-                        <button className={cx('more-btn')}>
-                            <FontAwesomeIcon icon={faEllipsisVertical} />
-                        </button>
+                <div className={cx('actions')}>
+                    {currentUser ? (
+                        <div className={cx('current-user')}>
+                            <Button text leftIcon={<FontAwesomeIcon icon={faPlus} />}>Upload</Button>
+                        </div>
+                    ) : (
+                        <>
+                            <Button text leftIcon={<FontAwesomeIcon icon={faPlus} />}>Upload</Button>
+                            <Button primary>Login</Button>
+                        </>
+                    )}
+
+                    <Menu items={currentUser ? userMenu : MENU_ITEMS} onChange={handleMenuChange}>
+                        {currentUser ? (
+                            <img className={cx('user-avatar')} src='https://fullstack.edu.vn/static/media/f8-icon.18cd71cfcfa33566a22b.png' alt='HungKM' />
+                        ) : (
+                            <button className={cx('more-btn')}>
+                                <FontAwesomeIcon icon={faEllipsisVertical} />
+                            </button>
+                        )}
                     </Menu>
                 </div>
             </div>
-        </header>
+        </header >
     );
 }
 
--- src/components/Popper/Menu/index.js
+++ src/components/Popper/Menu/index.js
@@ -32,6 +32,7 @@ function Menu({ children, items = [], onChange = defaultFn }) {
     return (
         <Tippy
             delay={[0, 700]}
+            offset={[10, 10]}
             interactive
             placement='bottom-end'
             render={attrs => (
@@ -44,6 +45,7 @@ function Menu({ children, items = [], onChange = defaultFn }) {
                     </PopperWrapper>
                 </div>
             )}
+            onHide={()=> setHistory(prev => prev.slice(0, 1))}
         >
             {children}
         </Tippy>
