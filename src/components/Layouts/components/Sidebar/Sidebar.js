4165428ce938de25ec26db4455517434f7f82e54
khonghung <ahihimaianh3@gmail.com>
2023-04-05 17:40:43 +0700

    UI sidebar 1

--- src/components/Layouts/components/Sidebar/Sidebar.js
+++ src/components/Layouts/components/Sidebar/Sidebar.js
@@ -1,11 +1,20 @@
 import classNames from 'classnames/bind';
-import styles from './Sidebar.module.scss'
+import styles from './Sidebar.module.scss';
+import Menu, { MenuItem } from './Menu';
+import config from '~/config';
+import { HomeIcon, FollowingIcon, LiveIcon } from '~/components/Icons';
 
 const cx = classNames.bind(styles);
 
 function Sidebar() {
     return (
-        <h2 className={cx('wrapper')}>Sidebar</h2>
+        <aside className={cx('wrapper')}>
+            <Menu>
+                <MenuItem title='For You' to={config.routes.home} icon={<HomeIcon />}/>
+                <MenuItem title='Following' to={config.routes.following} icon={<FollowingIcon />}/>
+                <MenuItem title='Live' to={config.routes.live} icon={<LiveIcon />}/>
+            </Menu>
+        </aside>
     );
 }
 
